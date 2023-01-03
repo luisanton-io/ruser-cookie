f190b8d09e858795beed8161af54efe1ffb3760e
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-01-03 15:01:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const { uuid } = require('uuidv4')
+const cors = require('cors')
 const app = express()
 
+const whitelist = [
+    'http://localhost:3000'
+]
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.get('/api/ruserid', (req, res) => {
     const _uuid = uuid()
     res.cookie('ruserid', _uuid, {
         maxAge: 1000 * 60 * 60 * 24 * 30,
         path: '/',
+        domain: '.reasonsecurity.com',
         sameSite: 'None',
         httpOnly: true,
         secure: true,
